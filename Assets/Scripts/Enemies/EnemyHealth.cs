489cce2b4bb74b00af1693ad9c3ad9d72f125b72
AbuniDaniel <daniel.abuni@s.unibuc.ro>
2022-04-16 16:41:19 +0300

    inventory

--- Assets/Scripts/Enemies/EnemyHealth.cs
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -33,7 +33,7 @@ public class EnemyHealth : MonoBehaviour
         }
         healthBar.SetHealth(currentHealth);
     }
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.tag == "Pickaxe"){
             TakeDamage(1);
