bfcb0e77c127c96cbf9d545f0cad3b20f786765e
AbuniDaniel <daniel.abuni@s.unibuc.ro>
2022-05-20 18:10:10 +0300

    .

--- Assets/GameOverScreen.cs
+++ Assets/GameOverScreen.cs
@@ -7,8 +7,12 @@ public class GameOverScreen : MonoBehaviour
 {
     public void RestartButton( )
     {   
-        
         SceneManager.LoadScene("GameScene");
+    }
+
+    public void MainMenuButton( )
+    {   
+        SceneManager.LoadScene("Menu");
     }
 
 }
